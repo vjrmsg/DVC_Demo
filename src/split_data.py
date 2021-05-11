f6729357231427a3f39d1c46f5c00bb929e85109
Sriramoju <Vijay.Sriramoju.CTR@doit.ri.gov>
2021-05-11 19:55:44 -0400

    all changes

--- src/split_data.py
+++ src/split_data.py
@@ -5,7 +5,7 @@ import argparse
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from get_data import read_params
-
+from load_data import read_params
 def split_and_saved_data(config_path):
     config = read_params(config_path)
     test_data_path = config["split_data"]["test_path"] 
