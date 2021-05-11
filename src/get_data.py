eda563efc9067adc1795a4189de0c54e17b109e0
Sriramoju <Vijay.Sriramoju.CTR@doit.ri.gov>
2021-05-11 15:10:13 -0400

    Stage 1 complete

--- src/get_data.py
+++ src/get_data.py
@@ -2,29 +2,26 @@
 ## process
 ## return dataframe
 import os
-import sys
-import yaml #no istallation required.
+import yaml
 import pandas as pd
 import argparse
 
 def read_params(config_path):
     with open(config_path) as yaml_file:
         config = yaml.safe_load(yaml_file)
-        print(config)
     return config
 
 def get_data(config_path):
     config = read_params(config_path)
-    #print(config)
+    # print(config)
     data_path = config["data_source"]["s3_source"]
-    df = pd.read_csv(data_path,sep=",", encoding='utf-8')
-    print(df.head())
+    df = pd.read_csv(data_path, sep=",", encoding='utf-8')
     return df
+
+
 
 if __name__=="__main__":
     args = argparse.ArgumentParser()
-    args.add_argument("--config",default="params.yaml")
+    args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
-    data=get_data(config_path=parsed_args.config) 
-
-
+    data = get_data(config_path=parsed_args.config)
