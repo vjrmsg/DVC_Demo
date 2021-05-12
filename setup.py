c58d396758db7a6c34b1ee531779699945b1d442
Sriramoju <Vijay.Sriramoju.CTR@doit.ri.gov>
2021-05-11 22:29:10 -0400

    setup file creation and tox

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="src", 
+    version="0.0.1", 
+    description="it is a wine Q package",
+    author="sv",
+    packages=find_packages(),
+    linces="MIT"
+    )
+
