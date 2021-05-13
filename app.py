0490b02e9c9dac368e94db2e32b236831f09ff44
Sriramoju <Vijay.Sriramoju.CTR@doit.ri.gov>
2021-05-13 13:59:08 -0400

    All updates

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ static_dir = os.path.join(webapp_root, "static")
 template_dir = os.path.join(webapp_root, "templates")
 
 app = Flask(__name__, static_folder=static_dir,template_folder=template_dir)
+############################################
 
+###################################################
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -33,6 +35,9 @@ def index():
             return render_template("404.html", error=error)
     else:
         return render_template("index.html")
+
+
+        
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
